ab8e46821027dd53d54ea63f08508b5e976316e8
Anup Kumar <anupkumar2k22@gmail.com>
2025-11-07 23:31:39 +0530

    Time: 0 ms (100%), Space: 14.7 MB (51.54%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+ unordered_map<int, int> mp; // value -> index
+
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (mp.find(diff) != mp.end()) {
+                return {mp[diff], i}; // Found the pair
+            }
+            mp[nums[i]] = i;
+        }
+
+        return {}; // Just for formality; guaranteed one valid answer exists
+    }
+};
