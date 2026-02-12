fadad3d44883be6a2c12f69df92022cc1b931020
Anup Kumar <anupkumar2k22@gmail.com>
2026-02-12 10:49:53 +0530

    Time: 4 ms (20.9%), Space: 62.9 MB (77.69%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        //BRUTE FORCE APPROACH(TLE)-------------------------------------------- 
+        // int maxWater = 0;
+        // for(int i =0;i<height.size();i++){
+        //     for(int j=i+1;j<height.size();j++){
+        //         int w=j-i;
+        //         int ht=min(height[j],height[i]);
+        //         int currWater=w*ht;
+        //         maxWater=max(currWater,maxWater);
+        //     }
+        // }return maxWater;
+        
+        // OPTIMAL APPROACH-----------------------------------------------------
+
+        int lp=0;
+        int rp=height.size()-1;
+        int maxWater=0;
+        while(lp<rp){
+            int w=rp-lp;
+            int ht=min(height[lp],height[rp]);
+            int currWater=w*ht;
+            maxWater=max(currWater,maxWater);
+            height[lp]<height[rp] ? lp++ : rp--;
+        }
+        return maxWater;
+    }
+};
