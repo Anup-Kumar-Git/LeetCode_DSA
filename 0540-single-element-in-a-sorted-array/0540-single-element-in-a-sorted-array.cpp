bd7cdbad01f3c31fc0bed15e7b21e4c633ac112c
Anup Kumar <anupkumar2k22@gmail.com>
2026-02-17 12:31:20 +0530

    Time: 0 ms (100%), Space: 26.1 MB (67.95%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -7,23 +7,23 @@ public:
         int end = n-1;
         while(start<=end){
             int mid= start +(end-start)/2;
-            if(mid == 0 && nums[0] != nums[1]) return nums[mid];
+            if(mid == 0 && nums[0] != nums[1]) return nums[mid];  //if mid = 1st element
             
-            if(mid == (n-1) && nums[n-1] != nums[n-2]) return nums[mid];
+            if(mid == (n-1) && nums[n-1] != nums[n-2]) return nums[mid];  //if mid = last element
 
-            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]){
+            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]){   //if mid is single element in array
                 return nums[mid];
-            }if(mid%2==0){
-                if(nums[mid-1]==nums[mid]){
+            }if(mid%2==0){            //even number of index
+                if(nums[mid-1]==nums[mid]){ //left array
                     end=mid-1;
-                }else {
+                }else {                     //right array
                     start=mid+1;
                 }
-            }else{
+            }else{                    //odd number od index
                 if(nums[mid-1]==nums[mid]){
-                   start=mid+1;
+                   start=mid+1;          //right array
                 }else{
-                    end=mid-1;
+                    end=mid-1;           //left array
                 }
             }
         }
