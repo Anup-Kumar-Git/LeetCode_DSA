a38c70eb098877925789537678b6f6bf34d10ebc
Anup Kumar <anupkumar2k22@gmail.com>
2026-03-21 10:28:32 +0530

    Time: 0 ms (100%), Space: 10 MB (53.68%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
 
-    bool isAlphaNum(char ch){
-        if((ch >='0' && ch <='9') || (tolower(ch)>='a' && tolower(ch)<='z')){
-            return true;
-        }
-        return false;
-    }
+    // bool isAlphaNum(char ch){
+    //     if((ch >='0' && ch <='9') || (tolower(ch)>='a' && tolower(ch)<='z')){
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    // bool isPalindrome(string s) {
+    //     int st=0;
+    //     int end=s.length()-1;
+    //     while(st<end){
+    //         if(!isAlphaNum(s[st])){
+    //             st++;
+    //             continue;
+    //         }
+    //         if(!isAlphaNum(s[end])){
+    //             end--;
+    //             continue;
+    //         }
+    //         if(tolower(s[st])!=tolower(s[end])){
+    //             return false;
+    //         }
+    //         st++;
+    //         end--;
+    //     }
+    //     return true;
+        
+    // }
+
+
     bool isPalindrome(string s) {
         int st=0;
         int end=s.length()-1;
         while(st<end){
-            if(!isAlphaNum(s[st])){
+            if(!isalnum(s[st])){
                 st++;
                 continue;
             }
-            if(!isAlphaNum(s[end])){
+            if(!isalnum(s[end])){
                 end--;
                 continue;
             }
