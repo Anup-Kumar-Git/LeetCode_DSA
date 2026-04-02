5fb5d5ddfd2059a55475fa185e04f5a145ea6604
Anup Kumar <anupkumar2k22@gmail.com>
2026-04-02 09:22:29 +0530

    Time: 0 ms (100%), Space: 14.8 MB (52.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> mp; // value -> index
 
-        for (int i = 0; i < nums.size(); i++) {
-            int diff = target - nums[i];
-            if (mp.find(diff) != mp.end()) {
-                return {mp[diff], i}; // Found the pair
+        //BRUTE FORCE APPROACH-----------------------------------------------
+        // vector<int>ans;
+        // for(int i=0;i<nums.size();i++){
+        //     int frist=nums[i];
+        //     for(int j=i+1;j<nums.size();j++){
+        //         int second=nums[j];
+        //         int sum=frist+second;
+        //         if(target==sum){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //         }
+        //     }
+        // }
+        // return ans;
+
+
+
+        //OPTIMAL APPROACH------------------------------------------------------
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            int frist=nums[i];
+            int sec=target-frist;
+            if(mp.find(sec)!=mp.end()){
+                ans.push_back(i);
+                ans.push_back(mp[sec]);
+                break;
             }
-            mp[nums[i]] = i;
+            mp[frist]=i;
         }
-
-        return {}; // Just for formality; guaranteed one valid answer exists
+        return ans;
     }
+
 };
