f1a00e507da9b8821f907d70e9b4e37867b6fc61
Anup Kumar <anupkumar2k22@gmail.com>
2026-04-07 09:32:28 +0530

    Time: 14 ms (23.13%), Space: 7.7 MB (87.85%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0 || n==1) {  //base case
+            return n;
+        }
+
+        return fib(n-1) + fib(n-2);  //function calling recursivly.
+    }
+};
