7bd445a926f31fc5727fcea59cefcb6e2e0e5b0c
Anup Kumar <anupkumar2k22@gmail.com>
2026-06-02 16:44:57 +0530

    Time: 8 ms (39.76%), Space: 54.7 MB (48.71%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -12,7 +12,6 @@ public:
         // return ans;
 
         int n=nums.size();
-        sort(nums.begin(),nums.end());
         vector<int>ans;
         vector<int>freq(n+1,0);
         for(int i=0;i<n;i++){
