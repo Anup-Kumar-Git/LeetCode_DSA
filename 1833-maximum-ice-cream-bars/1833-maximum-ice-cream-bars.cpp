a3218be53fd8a4e1cff64c531d7c6a66ed706032
Anup Kumar <anupkumar2k22@gmail.com>
2026-06-21 14:32:45 +0530

    Time: 37 ms (37.14%), Space: 80.2 MB (89.61%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -22,6 +22,8 @@ public:
             if(coins >= costs[i]){
                 coins -=costs[i];
                 count++;
+            }else{
+                break;
             }
         }return count;      
     }
