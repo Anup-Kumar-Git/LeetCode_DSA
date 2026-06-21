9839fee47e02d39273294f67bae512183bf54869
Anup Kumar <anupkumar2k22@gmail.com>
2026-06-21 14:32:11 +0530

    Time: 28 ms (70.01%), Space: 80.3 MB (61.09%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        int ans=0;
-        int result=0;
+        // int ans=0;
+        // int result=0;
+        // sort(costs.begin(),costs.end());
+        // for(int i=0;i<costs.size();i++){
+        //     result += costs[i];
+        //     if(result > coins){
+        //         ans=i;
+        //         break;   
+        //     }
+        // }
+        // if(result <= coins){
+        //     return costs.size();
+        // }
+        // return ans; 
+
+        int count=0;
         sort(costs.begin(),costs.end());
         for(int i=0;i<costs.size();i++){
-            result += costs[i];
-            if(result > coins){
-                ans=i;
-                break;
-               
+            if(coins >= costs[i]){
+                coins -=costs[i];
+                count++;
             }
-        }
-        if(result <= coins){
-            return costs.size();
-        }
-        return ans;    
-        
-        
+        }return count;      
     }
 };
