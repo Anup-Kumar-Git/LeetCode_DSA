0a9acb8998a6a3ed004cecf4eeeeba4a088e2045
Anup Kumar <anupkumar2k22@gmail.com>
2026-07-25 18:40:10 +0530

    Time: 3 ms (15.04%), Space: 8.6 MB (89.66%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        vector<int> digits;
+        int first = 0, second = 0;
 
         while (n > 0) {
-            digits.push_back(n % 10);
+            int d = n % 10;
+
+            if (d >= first) {
+                second = first;
+                first = d;
+            } else if (d > second) {
+                second = d;
+            }
+
             n /= 10;
         }
 
-        sort(digits.begin(), digits.end());
-
-        int m = digits.size();
-        return digits[m - 1] * digits[m - 2];
+        return first * second;
     }
 };
+
+// USING VECTOR----------------------------------------
+// class Solution {
+// public:
+//     int maxProduct(int n) {
+//         vector<int> digits;
+
+//         while (n > 0) {
+//             digits.push_back(n % 10);
+//             n /= 10;
+//         }
+
+//         sort(digits.begin(), digits.end());
+
+//         int m = digits.size();
+//         return digits[m - 1] * digits[m - 2];
+//     }
+// };
